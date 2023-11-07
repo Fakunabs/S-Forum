04566a3f75f8eac1c2bf67d2d930e9d8df8d55ae
Xuan Thinh <gempro0501@gmail.com>
2023-11-07 13:42:56 +0700

    [Feat] Change App icon and Launch Screen

---
+++ S-Forum/S-Forum/Services/API/APiService.swift
@@ -0,0 +1,48 @@
+//
+//  APiService.swift
+//  S-Forum
+//
+//  Created by Fakunabs on 04/11/2023.
+//
+
+import Foundation
+import Alamofire
+
+enum HttpError : Error {
+    case invalidUrl
+}
+
+class APIService {
+    
+    static let shareInitial = APIService()
+    
+    func requestAPIData<T: Decodable>(from path: String, parameters: Parameters?, method: HTTPMethod, headers: HTTPHeaders?) async throws -> T {
+        
+        guard let url = URL(string: path) else {
+            throw HttpError.invalidUrl
+        }
+        let encoding: ParameterEncoding = URLEncoding.default
+        
+        return try await withUnsafeThrowingContinuation { continuation in
+            AF.request(url, method: method, parameters: parameters, encoding: encoding, headers: headers)
+            .validate()
+            .responseDecodable(of: T.self) { response in
+                switch(response.result) {
+                case .success(let data):
+                    switch response.response?.statusCode {
+                    case 200,201:
+                        continuation.resume(returning: data)
+                    case 500:
+                        print("Server Error")
+                        break
+                    default:
+                        print("Unknown Error")
+                        break
+                    }
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+}
--- S-Forum/S-Forum/Supporting/AppDelegate.swift
+++ S-Forum/S-Forum/Supporting/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         IQKeyboardManager.shared.enable = true
-        FirebaseApp.configure()
+        Thread.sleep(forTimeInterval: 1)
         return true
     }
 
